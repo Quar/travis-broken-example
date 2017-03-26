dc87ba36ed3e1e486f8dd6e4712d704419806230
Quar <winchell.qian@gmail.com>
2017-03-26 11:08:06 -0600

    add test use pymysql

--- simplepy/mysql.py
+++ simplepy/mysql.py
@@ -1,1 +1,20 @@
-#TODO
+import pymysql.cursors
+import yaml
+import pandas as pd
+
+default_cfg = {'cursorclass':pymysql.cursors.DictCursor}
+
+def read_config(cfg_file):
+    with open(cfg_file) as f:
+        cfg = yaml.load(f)['mysql']
+
+def get_conn(cfg_file):
+    user_cfg = read_config(cfg_file)
+    cfg = {**default_cfg, **(user_cfg or {})}
+    return pymysql.connect(**cfg)
+
+def query(conn, query):
+    with conn.cursor() as cursor:
+        cursor.execute(query)
+        result = cursor.fectchall()
+        return pd.DataFrame(result)
--- test/package_test.py
+++ test/package_test.py
@@ -1,12 +1,28 @@
 import unittest
 import numpy as np
+from simplepy import mysql
+import os
+
+CONFIG_FILE = "./test_config.yml"
+cwd = os.path.basename(os.path.dirname(os.path.realpath(__file__)))
+cfg_file = os.path.join(cwd, CONFIG_FILE)
+
 
 class TestPackage(unittest.TestCase):
+
     def test_dot_product(self):
         RES = 285
         a = np.array(range(10))
         res = np.dot(a, a)
         self.assertEqual(res, RES)
+
+    def test_mysql(self):
+        conn = mysql.get_conn(cfg_file)
+        qry = """SELECT 2 + 3"""
+        RES = pd.DataFrame({"SELECT 2 + 3":[5]})
+        res = mysql.query(conn, qry)
+        self.assertTrue(res.equals(RES))
+
 
 if __name__ == "__main__":
     unittest.main()
